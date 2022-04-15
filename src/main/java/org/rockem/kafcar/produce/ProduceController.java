2615ec9652b6570fb0461517e6dce437aa469027
Eli Segal <eli.segal@gmail.com>
2022-04-15 12:45:37 +0300

    Added CI workflow

--- src/main/java/org/rockem/kafcar/produce/ProduceController.java
+++ src/main/java/org/rockem/kafcar/produce/ProduceController.java
@@ -3,9 +3,6 @@ package org.rockem.kafcar.produce;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
-import org.springframework.util.concurrent.ListenableFuture;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,30 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProduceController {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private transient final KafkaTemplate<String, String> kafkaTemplate;
+    private transient final ObjectMapper objectMapper = new ObjectMapper();
+
     public ProduceController(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     @PostMapping("/api/v1/produce")
     public void produceMessage(@RequestBody ProduceMessageRequest request) throws JsonProcessingException {
-        ListenableFuture<SendResult<String, String>> future =
-                kafkaTemplate.send(request.getTopic(), objectMapper.writeValueAsString(request.getValue()));
-
-        future.addCallback(new ListenableFutureCallback<>() {
-
-            @Override
-            public void onSuccess(SendResult<String, String> result) {
-                System.out.println("Sent message=[" + request.getValue() +
-                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
-            }
-
-            @Override
-            public void onFailure(Throwable ex) {
-                System.out.println("Unable to send message=["
-                        + request.getValue() + "] due to : " + ex.getMessage());
-            }
-        });
+        kafkaTemplate.send(request.getTopic(), objectMapper.writeValueAsString(request.getValue()));
     }
 }
